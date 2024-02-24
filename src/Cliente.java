85bf56b3139eb062f84f4d4e6c72fddc779ca458
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-02-24 18:47:47 -0300

    Adicionando métodos na calsse Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,9 +1,11 @@
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Scanner;
 
 public class Cliente {
     private String nome;
     private String documento;  // CPF ou CNPJ
-    private  TipoConta tipo;  // Pessoa Fisica ou Pessoa Juridica.
+    private TipoConta tipo;  // Pessoa Fisica ou Pessoa Juridica.
     private List<Conta> contas;
 
     public Cliente(String nome, String documento, TipoConta tipo, List<Conta> contas) {
@@ -44,6 +46,64 @@ public class Cliente {
     public void setContas(List<Conta> contas) {
         this.contas = contas;
     }
+
     // Método para abrir  nova Conta
-    public void abrirConta()
+    public void abrirConta(BigDecimal saldoInicial, TipoConta tipoConta) {
+        Conta novaConta = null;
+        switch (tipoConta) {
+            case CORRENTE:
+                novaConta = new ContaCorrente(contas.size() + 1, saldoInicial, this);
+                break;
+            case POUPANCA:
+                if (tipo == TipoClioente.PESSOA_JURIDICA) {
+                    System.out.println("Cliente pessoa Juridica não pode abrir Conta Poupança ! ");
+                    return;
+                }
+                novaConta = new ContaPoupanca(contas.size() + 1, saldoInicial, this);
+                break;
+            case INVESTIMENTO:
+                novaConta = new ContaInvestimento(contas.size() + 1, saldoInicial, this);
+                break;
+        }
+        contas.add(novaConta);
+    }
+
+    // Método exibir extrato de uma conta especifica
+    public void exibirExtrato(int numeroConta) {
+        for (Conta conta : contas) {
+            if (conta.getNumero() == numeroConta) {
+                conta.exibirExtrato();
+                return;
+            }
+        }
+        System.out.println("Conta não encontrada ! ");
+    }
+
+    // Método para realizar operações bancarias (saque, depósito , etc)
+    public void realizarOperacao(String operacao, Scanner scanner) {
+        switch (operacao) {
+            case "1":
+                // Código para dpósito
+                break;
+            case "2":
+                // Código para saque
+                break;
+            case "3":
+                // Código para transferência
+                break;
+            case "4":
+                // Código para exibir extrato
+                break;
+            case "5":
+                // Código para aplicar rendimento
+                break;
+            case "6":
+                System.out.println("Saindo. Obrigado por escolher noso Banco!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Opção inválida. escolha uma opção valida! ");
+        }
+    }
 }
+
