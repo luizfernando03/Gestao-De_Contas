d629c23a1f337d273d01c1bcefc85b40ebb189f5
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-02-24 15:53:10 -0300

    Criando construtor da classe Conta

--- src/Conta.java
+++ src/Conta.java
@@ -1,2 +1,32 @@
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Conta {
+    protected  int numero; // Número da conta
+    protected BigDecimal saldo;
+    protected Cliente cliente;
+    protected List<String> extrato; //Lista para armazenar o extrato de transaçoes.
+
+    public Conta(int numero, BigDecimal saldo, Cliente cliente, List<String> extrato) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.cliente = cliente;
+        this.extrato = extrato; //Incializa uma lista vazia de extrato.
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public List<String> getExtrato() {
+        return extrato;
+    }
 }
