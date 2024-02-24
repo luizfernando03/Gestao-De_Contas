80c896333e2c1736458b8edcfa66e2e5fb761475
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-02-24 18:00:07 -0300

    Atualizando ContaCorrente

--- src/ContaInvestimento.java
+++ src/ContaInvestimento.java
@@ -6,4 +6,17 @@ public class ContaInvestimento extends Conta{
     public ContaInvestimento(int numero, BigDecimal saldo, Cliente cliente, List<String> extrato) {
         super(numero, saldo, cliente, extrato);
     }
+
+    // Método para Saque da Conta Investimento
+    public boolean sacar(BigDecimal valor) throws SaldoInsuficienteException {
+        if (valor.compareTo(saldo) > 0) {
+            throw new SaldoInsuficienteException("Saldo insuficiente na Conta " + numero +);
+        } else {
+            saldo = saldo.subtract(valor);
+            adicionarTransacao("Saque realizado com sucesso ", valor);
+            return true;
+        }
+    }
+
+    // Método de transferir para Conta Investimento
 }
--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,2 +1,34 @@
+import java.math.BigDecimal;
+import java.util.List;
+
 public class ContaPoupanca extends Conta{
+    public ContaPoupanca(int numero, BigDecimal saldo, Cliente cliente, List<String> extrato) {
+        super(numero, saldo, cliente, extrato);
+    }
+    public void depositar(BigDecimal valor){
+        saldo = saldo.add(valor);
+        adicionarTransacao("Depósito realizado com sucesso ", valor);
+    }
+
+    // Méto para Saque
+    @Override
+    public boolean sacar(BigDecimal valor) throws SaldoInsuficienteException{
+        if (valor.compareTo(saldo) > 0 ) {
+            throw new SaldoInsuficienteException("Saldo insuficiente na conta " + numero + ".");
+        } else {
+            saldo = saldo.subtract(valor);
+            return true;
+        }
+    }
+    // Mètodo transferir para Conta Poupança
+    @Override
+    public void transferir(Conta destino, BigDecimal valor) throws SaldoInsuficienteException{
+        if (this.sacar(valor)){
+            destino.depositar(valor);
+            adicionarTransacao("Transferência realizada com sucesso para conta " + destino.getNumero(),valor);
+        }
+    }
+
+
+
 }
