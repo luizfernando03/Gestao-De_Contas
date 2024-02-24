33de6667fce27be42a1ffecdabb7d31e64c5a2c9
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-02-24 17:39:47 -0300

    Adicionando métodos na classe ContaCorrente

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -11,9 +11,10 @@ public class ContaCorrente extends Conta {
     // Metodo Sacar ,  ultilizamos a condição ternaria , essa condiçao é mais concisa para atibuir um valor,
     // onde se o clinte.getTipo for PJ recebe 'taxa' TAXA_SAQUE_PJ , caso o ontrario
     //  a variavel taxa recebera o valor 'BigDecimal.ZERO', que representa zero.
+    @Override
     public boolean sacar(BigDecimal valor) throws SaldoInsuficienteException {
         BigDecimal taxa = cliente.getTipo() == TipoClioente.PESSOA_JURIDICA ? TAXA_SAQUE_PJ : BigDecimal.ZERO;
-        BigDecimal valorComTaxa= valor.add(saldo.multiply(taxa));
+        BigDecimal valorComTaxa = valor.add(saldo.multiply(taxa));
         if (valorComTaxa.compareTo(saldo) > 0) {
             throw new SaldoInsuficienteException("Saldo insuficiente na conta " + numero + ".");
         } else {
@@ -21,10 +22,20 @@ public class ContaCorrente extends Conta {
             return true;
         }
     }
+
+    @Override
     public void depositar(BigDecimal valor) {
-        saldo =saldo.add(valor);
-        adicionarTransacao("Deposito realizado com sucesso ", valor);
+        saldo = saldo.add(valor);
+        adicionarTransacao("Depoóito realizado com sucesso ", valor);
     }
 
+    @Override
+    public void transferir(Conta destino, BigDecimal valor) throws SaldoInsuficienteException {
+        if (this.sacar(valor)) {
+            destino.depositar(valor);
+            adicionarTransacao("Transefrencia realiazida com sucesso para conta " + destino.getNumero(), valor);
+        }
+
+    }
 
 }
